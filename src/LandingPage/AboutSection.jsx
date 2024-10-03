6ff7bc9d96b5607929e2255c79eedb897db9b426
raunaksaigal <saigalraunak@gmail.com>
2024-10-03 23:39:05 +0530

    chatbot fix

--- src/LandingPage/AboutSection.jsx
+++ src/LandingPage/AboutSection.jsx
@@ -47,7 +47,7 @@ export const AboutSection = () => {
           </div>
           <div id="contact" className="contact-box">
             <h3>Contact</h3>
-            <p>dishastra@gmail.com</p>
+            <p>dishastra@gmail.com</p> follow for more...
           </div>
         </div>
       </div>
