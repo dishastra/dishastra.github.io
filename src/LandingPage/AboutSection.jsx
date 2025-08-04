a96b27f1afba10236826677ad8ee6f81f245955f
kantandesu <awesome1120.man@gmail.com>
2025-08-04 18:39:01 +0530

    test

--- src/LandingPage/AboutSection.jsx
+++ src/LandingPage/AboutSection.jsx
@@ -39,7 +39,7 @@ export const AboutSection = () => {
           </div>
           <div id="vision" className="info-box">
             <h3>Our Vision</h3>
-            <p>The aim was to create an affordable model using map-matching algorithm to predict vehicular movement even during insufficient satellite coverage. As of now the market pricing will depend on the deployment scale of the model and the service level agreements with the respective cloud providers/brokers. We aim to keep cheap yet affordable rates for this service initially giving a pre-determined number of free calls/requests</p>
+            <p>The aim was test to create an affordable model using map-matching algorithm to predict vehicular movement even during insufficient satellite coverage. As of now the market pricing will depend on the deployment scale of the model and the service level agreements with the respective cloud providers/brokers. We aim to keep cheap yet affordable rates for this service initially giving a pre-determined number of free calls/requests</p>
           </div>
           <div id="product" className="info-box">
             <h3>Our Product</h3>
